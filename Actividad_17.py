bb40e9e97904048174fafac231aa2f10731a77ed
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-08-12 01:45:13 -0600

    FINAL commits modificaciones

--- Actividad_17.py
+++ Actividad_17.py
@@ -63,6 +63,81 @@ class Inventario:
         else:
             print("El inventario está vacío.")
 
+#funcion solo para mostrar el menu para llamarlo unicmaente cuando lo necesite
+def mostrar_menu():
+    print("""
+--- MENÚ INVENTARIO ---
+1. Agregar producto
+2. Eliminar producto
+3. Eliminar último producto agregado
+4. Buscar producto
+5. Contar producto
+6. Ordenar productos
+7. Mostrar todos los productos
+8. Salir
+""")
+#instancio mi propia clase por asi decirlo, pq una clase es como una funcion master
+# que esta compuesta por funciones pequeñas que se llaman "metodos"
+#como la usamremos dentro del codigo, solo la llamaremos por la variable "inventario" pq
+# ya le asignamos la clase así "inventario = Inventario()"
+inventario = Inventario()
+
+while True:
+    mostrar_menu()
+    try:
+        opcion = int(input("Elige una opción (1-8): "))
+    except ValueError:
+        print("Por favor, ingresa un número válido.")
+        continue
+#dentro del match para evitar erroes de "DEDAZOS" usamos la funcion .strip() que unicamente
+# sirve para eliminar espacios extras a ambos lados de una cadena, por si el usuario mete un space en una
+# opcion, asi evitamos errores de comparacion por numero de espacios, si ese fuera el caso
+    match opcion:
+        case 1:
+            producto = input("Nombre del producto a agregar: ").strip()
+            if producto:
+                inventario.agregar_producto(producto)
+            else:
+                print("El nombre no puede estar vacío.")
+
+        case 2:
+            producto = input("Nombre del producto a eliminar: ").strip()
+            if producto:
+                inventario.eliminar_producto(producto)
+            else:
+                print("El nombre no puede estar vacío.")
+
+        case 3:
+            inventario.eliminar_ultimo_producto()
+
+        case 4:
+            producto = input("Nombre del producto a buscar: ").strip()
+            if producto:
+                inventario.buscar_producto(producto)
+            else:
+                print("El nombre no puede estar vacío.")
+
+        case 5:
+            producto = input("Nombre del producto para contar: ").strip()
+            if producto:
+                inventario.contar_producto(producto)
+            else:
+                print("El nombre no puede estar vacío.")
+
+        case 6:
+            inventario.ordenar_productos()
+
+        case 7:
+            inventario.listar_productos()
+
+        case 8:
+            print("Saliendo del sistema... ¡Gracias!")
+            break
+
+        case _:
+            print("Opción inválida, intenta nuevamente.")
+
+
 
 
 
